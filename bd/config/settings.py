e1f0319cf8c34346db12d2a7edc26f6666c7040e
Juliette Debono <juliette.debono@telecom-sudparis.eu>
2024-12-16 03:06:19 +0100

    deploy: modification du docker-compose

--- bd/config/settings.py
+++ bd/config/settings.py
@@ -132,8 +132,8 @@ STATIC_ROOT = BASE_DIR / 'main' / STATIC_URL
 LOGS_FILE = BASE_DIR / 'logs' / 'app.logs'
 
 # Répertoire pour les fichiers uploadés
-MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
