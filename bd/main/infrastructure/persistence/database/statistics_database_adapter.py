3ab5995d6768ff1d40cc15081335a69cde88dbef
Jul SQL <julsql1@gmail.com>
2025-05-22 22:20:43 +0200

    hexa(statistics): Réécriture du module Statistics en architecture hexagonale

--- bd/main/infrastructure/persistence/database/statistics_database_adapter.py
+++ bd/main/infrastructure/persistence/database/statistics_database_adapter.py
@@ -1,15 +1,14 @@
-from abc import ABC
-
 from django.db.models import Count, Sum, IntegerField, FloatField
-from django.db.models.functions import Cast, Round, Coalesce
+from django.db.models.functions import Cast, Coalesce, Round
 
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.statistics_repository import StatisticsRepository
 from main.infrastructure.persistence.database.models import BD
-from main.core.statistics.statistics_database_repository import StatisticsDatabaseRepository
 
 
-class StatisticsDatabaseConnexion(StatisticsDatabaseRepository, ABC):
-    def get_information(self):
-        return BD.objects.aggregate(
+class StatisticsDatabaseAdapter(StatisticsRepository):
+    def get_database_statistics(self) -> Statistics:
+        stats = BD.objects.aggregate(
             nombre=Count('id'),
             pages=Coalesce(Sum(Cast('number_of_pages', output_field=IntegerField())), 0),
             prix=Coalesce(
@@ -22,4 +21,13 @@ class StatisticsDatabaseConnexion(StatisticsDatabaseRepository, ABC):
                 output_field=IntegerField()
             ),
             tirage=Coalesce(Sum(Cast('deluxe_edition', output_field=IntegerField())), 0),
+        )
+
+        return Statistics(
+            nombre_albums=stats['nombre'],
+            nombre_pages=stats['pages'],
+            prix_total=stats['prix'],
+            nombre_editions_speciales=stats['tirage'],
+            nombre_dedicaces=0,
+            nombre_exlibris=0
         )
