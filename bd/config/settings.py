8893b15d17d5ee95bd9e5e768c4ac0887de8ee3d
Jul SQL <julsql1@gmail.com>
2025-06-19 20:54:43 +0200

    test(add-album): Test add album view

--- bd/config/settings.py
+++ bd/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '51.222.136.185', 'bd.julsql.fr', 'bd']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '51.222.136.185', 'bd.julsql.fr', 'bd', 'testserver']
 
 # Application definition
 
